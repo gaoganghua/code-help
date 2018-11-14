a65108d1169e7a8196d082efdabd16e9394aabe3
gaoganghua <gaoganghua@gxb.io>
2018-11-14 22:02:34 +0800

    Initial commit

---
+++ src/main/java/com/code/help/HelpApplication.java
@@ -0,0 +1,12 @@
+package com.code.help;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpApplication.class, args);
+    }
+}
